6d429591c5676120cd4491c6ee57097a64133061
Tom Swinkels <tom.swinkels@graphlr.io>
2019-09-19 13:58:43 +0200

    no message

---
+++ src/Prepr.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Graphlr\Prepr;
+
+use GuzzleHttp\Client;
+
+class Prepr
+{
+    protected $url;
+    protected $query;
+    protected $callableUrl;
+    protected $method = 'GET';
+    protected $params = [];
+
+    public function __construct()
+    {
+        $this->client = new Client([
+//            'base_uri' => config('prepr.base_url'),
+            'http_errors' => false,
+            'headers' => config('prepr.headers')
+        ]);
+
+        $this->baseUrl = config('prepr.base_url');
+    }
+
+    protected function call()
+    {
+        $request = $this->client->request($this->method, $this->callableUrl.$this->query, [
+            'form_params' => $this->params,
+        ]);
+
+        return json_decode($request
+            ->getBody()
+            ->getContents(), true);
+    }
+
+    protected function setGetUrl($url = null)
+    {
+        $this->method = 'GET';
+        $this->callableUrl = $this->baseUrl.$url;
+
+        return $this;
+    }
+
+    public function setPostUrl($url)
+    {
+        $this->method = 'POST';
+        $this->callableUrl = $this->baseUrl.$url;
+
+        return $this;
+    }
+
+    public function setDeleteUrl($url)
+    {
+        $this->method = 'DELETE';
+        $this->callableUrl = $this->baseUrl.$url;
+
+        return $this;
+    }
+
+    protected function addQuery(array $array)
+    {
+        $this->query = '?'.http_build_query($array);
+
+        return $this;
+    }
+
+    protected function setParams(array $array)
+    {
+        $this->params = $array;
+
+        return $this;
+    }
+}
---
+++ src/config.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Base Url
+    |--------------------------------------------------------------------------
+    |
+    | The base url of the API to consume.
+    |
+    */
+
+    'base_url' => env('PREPR_URL', 'https://api.tom.graphlr.de'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | HTTP Headers
+    |--------------------------------------------------------------------------
+    |
+    | The HTTP headers to be send along in each call
+    |
+    */
+
+    'headers' => [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'Authorization' => env('PREPR_TOKEN', ''),
+    ]
+
+];
